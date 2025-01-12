e15e1ecf62424516b8af06d5fc3aa007b735fe7a
ItsMeSwagat <xtha.swagat69@gmail.com>
2025-01-12 14:27:53 +0545

    social component responsiveness

--- app/components/SocialComp.tsx
+++ app/components/SocialComp.tsx
@@ -1,43 +1,46 @@
 import Image from "next/image";
 import user from "../assets/icons/profile-2user.png";
+import routing from "../assets/icons/routing.png";
+import likesicon from "../assets/icons/Exceeds.png";
+import staricon from "../assets/icons/star.png";
 
 const SocialComp = () => {
   return (
-    <section className="home_section py-[5rem] bg-secondary grid grid-flow-col justify-between">
-      <div>
+    <section className="home_section py-[5rem] bg-secondary grid grid-cols-2 md:grid-cols-4 gap-10 md:grid-flow-col justify-between">
+      <div className=" grid gap-4 justify-center items-center">
         <div className="bgpolygon">
-          <img src={user.src} alt="" width={60} className=" p-2" />
+          <img src={user.src} alt="" width={60} className="p-2" />
         </div>
         <div>
           <h1 className=" heading">28K</h1>
           <span>Total User</span>
         </div>
       </div>
-      <div>
+      <div className=" grid gap-4 justify-center items-center">
         <div className=" bgpolygon">
-          <img src={user.src} alt="" width={60} className=" p-2" />
+          <img src={routing.src} alt="" width={60} className=" p-2" />
         </div>
         <div>
-          <h1 className=" heading">28K</h1>
-          <span>Total User</span>
+          <h1 className=" heading">55K</h1>
+          <span>Total Tours</span>
         </div>
       </div>
-      <div>
+      <div className=" grid gap-4 justify-center items-center">
         <div className=" bgpolygon">
-          <img src={user.src} alt="" width={60} className=" p-2" />
+          <img src={likesicon.src} alt="" width={60} className=" p-2" />
         </div>
         <div>
-          <h1 className=" heading">28K</h1>
-          <span>Total User</span>
+          <h1 className=" heading">18K</h1>
+          <span>Social Likes</span>
         </div>
       </div>
-      <div>
+      <div className=" grid gap-4 justify-center items-center">
         <div className=" bgpolygon">
-          <img src={user.src} alt="" width={60} className=" p-2" />
+          <img src={staricon.src} alt="" width={60} className=" p-2" />
         </div>
         <div>
           <h1 className=" heading">28K</h1>
-          <span>Total User</span>
+          <span>5 Star Ratings</span>
         </div>
       </div>
     </section>
