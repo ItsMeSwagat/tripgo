f5738be4414aa48dbacbe75543ead6e53cdf4c8b
ItsMeSwagat <xtha.swagat69@gmail.com>
2025-01-19 11:13:28 +0545

    heading component changes

--- app/components/UI/Heading.tsx
+++ app/components/UI/Heading.tsx
@@ -5,17 +5,15 @@ type Props = {};
 
 export default function Heading({}: Props) {
   return (
-    <div className=" w-full h-80 bg-[url('/imagebackground.png')] bg-cover grid justify-center items-center">
-      <div className=" grid justify-center items-center">
-        <h1 className=" heading">About Us</h1>
-        <Breadcrumb
-          homeElement="Home"
-          separator={<span className=""> {">"} </span>}
-          activeClasses=" text-secondary"
-          containerClasses="flex items-center gap-4 text-white"
-          capitalizeLinks
-        />
-      </div>
+    <div className=" w-full h-80 bg-[url('/imagebackground.png')] bg-cover flex flex-col justify-center items-center">
+      <h1 className=" heading">About Us</h1>
+      <Breadcrumb
+        homeElement="Home"
+        separator={<span className=""> {">"} </span>}
+        activeClasses=" text-secondary"
+        containerClasses="flex items-center gap-4 text-white"
+        capitalizeLinks
+      />
     </div>
   );
 }
