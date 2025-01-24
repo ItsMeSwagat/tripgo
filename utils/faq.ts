69650862d8f4dc28756dad98a8746505f409328f
ItsMeSwagat <xtha.swagat69@gmail.com>
2025-01-24 12:18:39 +0545

    assets and imports added

---
+++ utils/faq.ts
@@ -0,0 +1,50 @@
+export const ShippingInfoFaQ = [
+  {
+    id: 1,
+    title: "When will my order ship?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 2,
+    title: "I'm having trouble with my part, what should I do?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 3,
+    title: "When will my order arrive?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 4,
+    title: "How long does it take to ship to me?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 5,
+    title: "Can you leave my order outside my door?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 6,
+    title: "What is a Refundable Core Deposit?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 7,
+    title: "Where do you ship to?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+  {
+    id: 8,
+    title: "How long do I have to send back my core?",
+    answer:
+      "We're always working to get your orders on their way to you as quickly as we can. Most orders will be dispatched within 2-4 business days, though there may be some delays around sales and peak periods. You'll get a notification when your order has started its journey.",
+  },
+];
